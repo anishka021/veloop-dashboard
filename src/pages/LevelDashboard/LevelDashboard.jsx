c8e37d3e34a5c5f69360de8714d138b52a35f477
Anishka Negi <anishkasinghnegi1234@gmail.com>
2026-09-17 16:01:54 +0530

    Update LevelDashboard.jsx

--- src/pages/LevelDashboard/LevelDashboard.jsx
+++ src/pages/LevelDashboard/LevelDashboard.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import styles from './LevelDashboard.module.css';
 import LevelHero from '../../components/LevelHero/LevelHero';
+import AchievementsStrip from '../../components/AchievementsStrip/AchievementsStrip';
 import CurrentLevel from '../../components/CurrentLevel/CurrentLevel';
 import NextLevelReward from '../../components/NextLevelReward/NextLevelReward';
 import LevelRoadmap from '../../components/LevelRoadmap/LevelRoadmap';
 import GameContainer from '../../components/PlayAndEarn/GameContainer/GameContainer';
-import EarnMoreXP from '../../components/EarnMoreXP/EarnMoreXP';
 import XPActivity from '../../components/XPActivity/XPActivity';
+import EarnMoreXP from '../../components/EarnMoreXP/EarnMoreXP';
 import SparkleTrail from '../../components/SparkleTrail/SparkleTrail';
+import CoinRain from '../../components/CoinRain/CoinRain';
 
 const LevelDashboard = () => {
   return (
     <div className={styles.page}>
       <SparkleTrail />
+      <CoinRain />
 
       <div className={styles.container}>
         <LevelHero />
+        <AchievementsStrip />
 
         <div className={styles.row2}>
           <CurrentLevel />
